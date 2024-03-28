dcf26fb78f45fab9ba1247aa7f0e9823421407a2
cardelbec <Kiolpor.kolopo@gmail.com>
2024-03-28 16:14:21 +0100

    drf-spectacular, re-migration

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('v1/hotel', Hotel_APIView.as_view()),
-    path('v1/flight', Hotel_APIView.as_view()),
-    path('v1/activity', Hotel_APIView.as_view()), 
+    path('hotel', Hotel_APIView.as_view()),
+    path('flight', Hotel_APIView.as_view()),
+    path('activity', Hotel_APIView.as_view()), 
 ]
--- web_project/settings.py
+++ web_project/settings.py
@@ -39,7 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
